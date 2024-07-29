ca2fc2f2d4b7c3c8300e8737fbbe7444045ef42b
masa9973 <masamichi.ishikawa19990730@gmail.com>
2024-07-29 16:36:55 +0900

    update nuxtconfig

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,15 +2,19 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    routeRules: {
-        "/": {
-            prerender: false,
-        },
-        "/hoge": {
-            prerender: process.env.HOGE_DIR_PRERENDERING === "true",
-        },
-        "/fuga": {
-            prerender: process.env.FUGA_DIR_PRERENDERING === "true",
+    hooks: {
+        "nitro:config"(nitroConfig) {
+            const piyoRenderingIDArr = [0, 1, 2]; // 存在するpiyoページのID
+            if (!nitroConfig.prerender) {
+                return;
+            } else {
+                console.log("routes", nitroConfig.prerender.routes);
+                nitroConfig.prerender.routes = piyoRenderingIDArr.map((item) => {
+                    return `/piyo/${item}`;
+                });
+                // /hogeと/piyo/0はprerenderingしない
+                nitroConfig.prerender.ignore = ["/hoge", "/piyo/0"];
+            }
         },
     },
 });
